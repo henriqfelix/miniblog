cf26d9cd8137e73126e51bb7f740f75ccb65f66a
henriqfelix <henriquefelix1821@gmail.com>
2022-07-31 13:06:10 -0300

    criar cadastro, autenticação e contexto do usuário

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// CONTEXT
+import { AutoProvider } from "./context/AuthContext";
+
 // PAGES
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
@@ -15,18 +18,20 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AutoProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AutoProvider>
     </div>
   );
 }
